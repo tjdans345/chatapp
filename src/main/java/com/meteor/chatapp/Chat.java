c1433691f0688caf70dd68a31187514eb7a1b4ad
tjdans345 <tjdans345@naver.com>
2021-10-04 17:25:13 +0900

    Initial commit

---
+++ src/main/java/com/meteor/chatapp/Chat.java
@@ -0,0 +1,15 @@
+package com.meteor.chatapp;
+
+
+import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+
+
+@Data
+@Document(collection = "chat")
+public class Chat {
+
+
+
+}
